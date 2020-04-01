9082a22754befc715571ae75f2042d8b1ca5028f
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-04-01 22:53:58 +0530

    Add files via upload

---
+++ firstProg.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var firstName = "Parul" 	//String
+var age = 10;				//integer
+var boo = true;				//boolean
+
+console.log(age);
+age = age + 10;
+console.log(age);
+
+console.log(lastName);
+var lastName;
+console.log(lastName);
+lastName = 'Mathur';
+console.log(lastName);
